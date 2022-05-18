44c79e2625ada9e719da4c40ebff2bf8a9e6d4e1
mazen Aldebeat <mazend76@gmail.com>
2022-05-19 00:07:16 +0200

    Project Introduction

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <header>Header</header>
+       <main>Main</main>
+       <footer>Footer</footer>
+     </div>
+  );
+}
+
+export default App;
